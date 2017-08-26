5c4c8cd0c918221e5c371c75c3a9c4f6e0c3963b
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-08-26 23:19:24 +0300

    add config file

---
+++ app/src/main/java/blesh/Config.java
@@ -0,0 +1,16 @@
+package blesh;
+
+/**
+ * @author Skylifee7 on 23/06/2017.
+ */
+
+public class Config {
+
+    // Blesh SDK Developer Key. Replace this key with your's.
+    protected static final String DEVELOPER_KEY = "notrealkey";
+
+    protected static final String DEVELOPER_UNAME= "notrealdevname";
+
+    protected static final String INTEGRATION_ID = "UID_2149437";
+
+}
