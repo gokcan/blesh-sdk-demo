60c40cf34bc823c6c8015eca4c3b63a569a8f263
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-08-28 20:16:38 +0300

    add onCreate

---
+++ app/src/main/java/com/blesh/demo/bleshapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.blesh.demo.bleshapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import blesh.BleshHelperService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        startService(new Intent(this, BleshHelperService.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        
+        if (id == R.id.action_help) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
